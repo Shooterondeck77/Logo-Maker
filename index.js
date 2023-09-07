b44638f5f9d247bf928beaa79d51262c938b281d
Shooterondeck77 <timothytaffur@gmail.com>
2023-09-06 20:54:00 -0400

    Shapes and questions

---
+++ index.js
@@ -0,0 +1,15 @@
+const { questions } = require('./lib');
+const { prompt } = require('inquirer');
+
+prompt(questions).then(ans => {
+    console.log(ans);
+})
+class Svg{
+constructor(){
+    this.textElement = ''
+    this.shapeElement = ''
+
+}
+render
+
+}
