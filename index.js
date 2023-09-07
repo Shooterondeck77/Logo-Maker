a8e01a2f33ca79aa838d0c8e3947042769cce06b
Shooterondeck77 <timothytaffur@gmail.com>
2023-09-06 21:08:28 -0400

    constructor and errors

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ constructor(){
     this.shapeElement = ''
 
 }
-render
-
+render (){
+    return <Svg version ="1.2" xmlns="http://www.w3.org/2000/svg" width= "300" height="200"></Svg> 
+}
+setTextElement(text,color){
+    this.textElement = <text x="150" y="125" font-size="60"></text>
+}
+setShapeElement(shape){
+this.shapeElement = shape.render()
+}
 }
